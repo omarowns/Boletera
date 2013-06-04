2ede3f2b03ea4330cf7a7f1dd5428a571199773b
Mario Gómez <mariogomezmtz.92@hotmail.com>
2013-06-04 11:51:17 -0500

    Conector MySQL con métodos para ABC

---
+++ MySQLConnector.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using MySql.Data.Entity;
+
+namespace MySQLTest
+{
+    class MySQLConnector
+    {
+        //Connection StringBuilder
+        private MySqlConnectionStringBuilder connData;
+        //Connection object
+        private MySqlConnection conn;
+        //Command
+        private MySqlCommand cmd;
+
+        //Getters and Setters
+        public MySqlConnectionStringBuilder ConnData { get { return connData; } }
+        public MySqlConnection Conn { get { return conn; }}
+
+        public MySQLConnector(string server = "localhost",string user="root",string database="world",uint port=3306,string password="sa123")
+        {
+            connData = new MySqlConnectionStringBuilder();
+            //Datos de la conexión
+            connData.Server = server;
+            connData.UserID = user;
+            connData.Database = database;
+            connData.Port = port;
+            connData.Password = password;
+
+            doConnect();
+        }
+
+        private void doConnect()
+        {
+            //Carga los datos de la conexión
+            conn = new MySqlConnection(connData.ToString());
+            try
+            {
+                //Abre la conexión
+                conn.Open();
+            }catch(MySqlException ex){
+                Console.WriteLine("Error en tiempo de ejecución: {0}", ex.Message);
+            }
+        }
+
+        public MySqlDataReader getReader(string query)
+        {
+            try
+            {
+                MySqlDataReader rdr;
+                cmd = new MySqlCommand(query,conn);
+                rdr = cmd.ExecuteReader();
+                return rdr;
+            }catch(MySqlException ex){
+                Console.WriteLine("Error: {0}",ex.Message);
+                return null;
+            }
+        }
+
+        public object getEscalar(string query)
+        {
+            try
+            {
+                Object obj;
+                cmd = new MySqlCommand(query, conn);
+                obj = cmd.ExecuteScalar();
+                return obj;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+                return null;
+            }
+        }
+
+        public void update(string query)
+        {
+            try
+            {
+                cmd = new MySqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+            }
+        }
+
+        
+      
+    }
+}
