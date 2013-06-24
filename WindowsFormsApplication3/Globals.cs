33b0ffa23e0fae8b38163371829a76927bf7ee4f
Omar Garcia <omarexperience@gmail.com>
2013-06-23 19:04:58 -0500

    mend

--- WindowsFormsApplication3/Globals.cs
+++ WindowsFormsApplication3/Globals.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Boletera{
     static class Globals
     {
-        public static MySQLConnector connector = new MySQLConnector();
+        public static MySQLConnector connector = new MySQLConnector("localhost","root","boletera",3306,"toor");
 
         public static string server;
         public static string user;
